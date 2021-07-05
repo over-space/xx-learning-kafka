79c6489482c5d9c2b39cf1dfce1156315052b497
lifang <lifang@jingkunsystem.com>
2021-07-05 17:49:12 +0800

    first commit

---
+++ src/main/java/com/learning/kafka/XxLearningKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxLearningKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxLearningKafkaApplication.class, args);
+    }
+
+}
